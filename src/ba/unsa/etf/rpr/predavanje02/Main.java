8ca0c00078166f2ee73fde3d97d3ffaa5716908f
goralija <hgoralja2@etf.unsa.ba>
2023-10-30 18:47:25 +0100

    first

---
+++ src/ba/unsa/etf/rpr/predavanje02/Main.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr.predavanje02;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner ulaz = new Scanner(System.in);
+        int n = ulaz.nextInt();
+        int m = ulaz.nextInt();
+        System.out.println("Prvi broj: " + n);
+        System.out.println("Drugi broj: " + m);
+    }
+
+}
